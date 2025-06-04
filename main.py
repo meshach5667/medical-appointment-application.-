ae4a0ea7edcc2c21d1d818d5c6eb3bbaad045de2
Obed Meshach <129974200+meshach5667@users.noreply.github.com>
2025-06-04 22:12:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ app = FastAPI()
 app.include_router(patient_router, prefix="/patients", tags=["patients"])
 app.include_router(doctor_router, prefix="/doctors", tags=["doctors"])
 app.include_router(appointment_router, prefix="/appointments", tags=["appointments"])
+
+
+app.get
